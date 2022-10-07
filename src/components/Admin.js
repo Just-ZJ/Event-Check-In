4f48afd6586c9d34801ccd8ee8caeb6928cc1d34
ZJ <tan.955@osu.edu>
2022-10-07 00:04:08 -0400

    added functionality to check people in

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,12 +1,57 @@
+import { getDatabase, ref, child, get } from "firebase/database";
+import { useState } from "react";
+
+import { Person } from "./Person";
+
+const dataRendering = (data) => {
+  return data == null ? (
+    <></>
+  ) : (
+    Object.keys(data).map((element) => {
+      return Object.keys(data[element]).map((people, i) => {
+        return (
+          <Person
+            key={i}
+            person={data[element][people]}
+            path={`names/${element}/${people}`}
+          />
+        );
+      });
+    })
+  );
+};
+
 export function Admin() {
+  // {onValue(names, (snapshot) => {
+  //   const data = snapshot.val();
+  //   for (let key in data) {
+  //     let element = data[key];
+  //     for (let person in element) {
+  //       console.log(element[person]);
+  //       return <Person person={person} />;
+  //     }
+  //   }
+  //   // updateStarCount(postElement, data);
+  // })}
+
+  const [data, setData] = useState(null);
+  const database = ref(getDatabase());
+  get(child(database, "names"))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        setData(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return (
     <>
       <div>This page is for admins to view</div>
+      {dataRendering(data)}
     </>
   );
 }
-
-// let name = "陈政吉";
-// let a = name.charCodeAt(0);
-// document.getElementById("tryout").innerText = a;
-// console.log(name.charAt(0));
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,3 +1,26 @@
-export function Person() {
-  return <></>;
+import { getDatabase, ref, set } from "firebase/database";
+
+export function Person({ person, path }) {
+  const onChangeEvent = () => {
+    console.log(`${person.name}'s checkbox clicked - ${person.checkin}`);
+    console.log(path);
+    const db = getDatabase();
+    set(ref(db, path), {
+      name: person.name,
+      checkin: true,
+    });
+  };
+
+  return (
+    <>
+      <div class="mb-3">
+        <label>{person.name}</label>
+        <input
+          type="checkbox"
+          checked={person.checkin}
+          onChange={() => onChangeEvent()}
+        />
+      </div>
+    </>
+  );
 }
