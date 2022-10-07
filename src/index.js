285581556c2c658f1c859e9b6fdd80d08db3b2c5
ZJ <tan.955@osu.edu>
2022-10-07 19:42:58 -0400

    added mdb icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 // ***** Start of Firebase Configurations *****
 import { initializeApp } from "firebase/app";
